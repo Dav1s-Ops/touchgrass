91cdad578991b9673cd5666b00e01c6517ff86b7
Dav1s-Ops <davisweimer@gmail.com>
2025-12-23 11:05:09 -0700

    Add basic timer loop w/ Spectre

--- touchgrass/Program.cs
+++ touchgrass/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+namespace Touchgrass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.Markup("[bold green]Welcome to Touchgrass Pomodoro Timer![/]\n");
+            var timer = new PomodoroTimer();
+
+            // parse args for custom durations e.g. tgrass --work 30 --break 10
+            if (args.Length > 0)
+            {
+                //TODO: Parse em!
+            }
+
+            RunTimer(timer);
+        }
+
+        static void RunTimer(PomodoroTimer timer)
+        {
+            timer.StartWork();
+
+            while (true)
+            {
+                var phase = timer.IsWorking ? "Work" : "Break";
+                AnsiConsole.Status()
+                    .Spinner(Spinner.Known.FingerDance)
+                    .Start($"{phase} session starting...", context =>
+                    {
+                        DetermineTimerPhase(timer);
+
+                        while (timer.RemainingTime > 0)
+                        {
+                            Thread.Sleep(1000);
+                            timer.Tick();
+                            context.Status($"[yellow]{phase}: {TimeSpan.FromSeconds(timer.RemainingTime):mm\\:ss}[/]");
+                            context.Refresh();
+                        }
+                    });
+
+                AnsiConsole.Markup($"[bold]{phase} complete![/]\n");
+                timer.SwitchPhase();
+                if (!AnsiConsole.Confirm("Continue to next phase?")) break;
+            }
+        }
+
+        public static void DetermineTimerPhase(PomodoroTimer timer)
+        {
+            if (timer.IsWorking)
+            {
+                timer.StartWork();
+            }
+            else
+            {
+                timer.StartBreak();
+            }
+        }
+    }
+}
