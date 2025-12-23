48fe4f115d437aee9fb2204019c052e2f9951610
Dav1s-Ops <davisweimer@gmail.com>
2025-12-23 12:39:25 -0700

    Add min sec util method

---
+++ touchgrass/PomodoroUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Touchgrass
+{
+    public static class TimeExtensions
+    {
+        public static string ToMinSecString(this int seconds, bool showHours = true, string separator = ":")
+        {
+            var time = TimeSpan.FromSeconds(seconds);
+            var hours = (int)time.TotalHours;
+
+            if (showHours && hours > 0)
+            {
+                return $"{hours}h {time:mm\\:ss}";
+            }
+
+            return $"{time:mm\\:ss}";
+        }
+    }
+}
