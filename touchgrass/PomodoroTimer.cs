19f606d159bedeab6ed46b52c2fb566889daf355
Dav1s-Ops <davisweimer@gmail.com>
2025-12-23 12:39:12 -0700

    Just always be a break based on duration

--- touchgrass/PomodoroTimer.cs
+++ touchgrass/PomodoroTimer.cs
@@ -8,8 +8,9 @@ namespace Touchgrass
 {
     public class PomodoroTimer
     {
-        public int WorkDuration { get; set; } = 25 * 60;
-        public int BreakDuration { get; set; } = 5 * 60;
+        public int Cycles { get; set; } = 4;
+        public int WorkDuration { get; set; } = 25 * 1;
+        public int BreakDuration { get; set; } = 5 * 1;
         public int CurrentCycle { get; set; } = 0;
         public int RemainingTime { get; private set; }
         public bool IsWorking { get; private set; }
@@ -41,7 +42,7 @@ namespace Touchgrass
 
         public void StartBreak()
         {
-            RemainingTime = (CurrentCycle % 4 == 0) ? 15 * 60 : BreakDuration;
+            RemainingTime = BreakDuration;
             IsWorking = false;
         }
     }
