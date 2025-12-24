8ffb783aa6eadad8c3386c823abd882de444573f
Dav1s-Ops <davisweimer@gmail.com>
2025-12-24 11:25:56 -0700

    Attach IPomodoroConfig

--- src/Touchgrass/Services/PomodoroConfig.cs
+++ src/Touchgrass/Services/PomodoroConfig.cs
@@ -2,7 +2,10 @@
 
 namespace Touchgrass.Services
 {
-    public class PomodoroConfig
+    public class PomodoroConfig : IPomodoroConfig
     {
+        public int Cycles { get; set; } = 4;
+        public int WorkDurationSeconds { get; set; } = 25 * 60;
+        public int BreakDurationSeconds { get; set; } = 5 * 60;
     }
 }
