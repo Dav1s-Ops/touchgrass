a727d6bf8272c124c4081e57bcfea4e49afb45c6
Dav1s-Ops <davisweimer@gmail.com>
2025-12-24 11:40:17 -0700

    Add primary constructor, props and methods

--- src/Touchgrass/Services/PomodoroTimer.cs
+++ src/Touchgrass/Services/PomodoroTimer.cs
@@ -2,8 +2,43 @@
 
 namespace Touchgrass.Services
 {
-    public class PomodoroTimer
+    public class PomodoroTimer(IPomodoroConfig config) : IPomodoroTimer
     {
+        private readonly IPomodoroConfig _config = config;
 
+        public int CurrentCycle { get; private set; } = 1;
+        public int RemainingTime { get; private set; }
+        public bool IsWorking { get; private set; }
+
+        public void StartWork()
+        {
+            RemainingTime = _config.WorkDurationSeconds;
+            IsWorking = true;
+        }
+
+        public void StartBreak()
+        {
+            RemainingTime = _config.BreakDurationSeconds;
+            IsWorking = false;
+        }
+
+        public void Tick()
+        {
+            if (RemainingTime > 0)
+                RemainingTime--;
+        }
+
+        public void SwitchPhase()
+        {
+            if (IsWorking)
+            {
+                StartBreak();
+            }
+            else
+            {
+                CurrentCycle++;
+                StartWork();
+            }
+        }
     }
 }
