ab7de26f2d0a3088bd4164c632768a0a8ebe0db5
Dav1s-Ops <davisweimer@gmail.com>
2025-12-26 11:41:57 -0700

    Handle hours

--- src/Touchgrass/Utils/TimeExtensions.cs
+++ src/Touchgrass/Utils/TimeExtensions.cs
@@ -5,7 +5,14 @@
         public static string ToMinSecString(this int seconds)
         {
             var ts = TimeSpan.FromSeconds(seconds);
-            return $"{ts.Minutes:D2}:{ts.Seconds:D2}";
+            if (ts.Hours > 0)
+            {
+                return $"{ts.Hours:D2}:{ts.Minutes:D2}:{ts.Seconds:D2}";
+            }
+            else
+            {
+                return $"{ts.Minutes:D2}:{ts.Seconds:D2}";
+            }
         }
     }
 }
